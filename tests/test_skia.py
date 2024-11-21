33f14238fa502b1d3127f0839a33b5ebac49293e
meetwq <meetwq@gmail.com>
2024-11-21 20:35:16 +0800

    adjust

--- tests/test_skia.py
+++ tests/test_skia.py
@@ -12,3 +12,38 @@ def test_import_skia_textlayout():
     from skia import textlayout
 
     print("test")
+
+
+def test_skia_paragraph():
+    from skia import Surfaces, ImageInfo, AlphaType
+    from skia import textlayout
+    from skia import ColorBLACK, ColorWHITE, FontMgr, FontStyle, Paint, Unicodes, kPNG
+
+    paint = Paint()
+    paint.setAntiAlias(True)
+    paint.setColor(ColorBLACK)
+
+    style = textlayout.TextStyle()
+    style.setFontSize(30.0)
+    style.setForegroundPaint(paint)
+
+    font_collection = textlayout.FontCollection()
+    font_collection.setDefaultFontManager(FontMgr())
+    para_style = textlayout.ParagraphStyle()
+    builder = textlayout.ParagraphBuilder.make(para_style, font_collection, Unicodes.ICU.Make())
+    builder.pushStyle(style)
+    builder.addText("test")
+
+    paragraph = builder.Build()
+    paragraph.layout(500.0)
+
+    surface = skia.Surfaces.MakeRasterN32Premul(500, 500)
+    canvas = surface.getCanvas()
+    canvas.clear(ColorWHITE)
+    paragraph.paint(canvas, 0, 0)
+
+    surface.flushAndSubmit()
+    image = surface.makeImageSnapshot()
+    image.save("test.png", kPNG)
+
+    print("test")
